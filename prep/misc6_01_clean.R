583f920c5813bce5756f890f0b7d4b9e8c0d31fb
tylersimko <tylersimko@gmail.com>
2022-07-01 15:00:06 -0400

    draft day 4

---
+++ prep/misc6_01_clean.R
@@ -0,0 +1,32 @@
+
+library(tidyverse)
+library(readstata13)
+
+# c_dat <- read.dta13("misc6/clean_fs_bangladesh.dta")
+
+countries <- c("ghana", "kosovo", "nepal", "nigeria", "mongolia",
+               "rwanda", "turkmenistan", "zimbabwe")
+
+files <- list.files("misc6")
+
+s <- sapply(countries, function(i) {
+  str_detect(files, i)
+}) %>% rowSums()
+
+to_read <- files[s == 1]
+
+all_countries <- lapply(to_read, function(i) {
+  read.dta13(paste0("misc6/", i)) %>% 
+    mutate(country = gsub("clean_fs_|.dta", "", i)) %>% 
+    select(country, highest_grade, 
+           male = boy, inschool, numeracy) %>% as_tibble()
+})
+
+df <- bind_rows(all_countries)
+
+to_write <- df %>% 
+  filter(highest_grade >= 1 & highest_grade <= 6)
+
+to_write$country <- tools::toTitleCase(to_write$country)
+
+write_csv(to_write, "lessons/data/mics6.csv")
